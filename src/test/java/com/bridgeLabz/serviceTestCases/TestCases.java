817d7b1d19b91ac7098b416b711f99674b728a41
vampire16 <siddheshsalvi525@gmai.com>
2020-03-17 12:58:16 +0530

    ADDED : New test method to check file name is correct or not

--- src/test/java/com/bridgeLabz/serviceTestCases/TestCases.java
+++ src/test/java/com/bridgeLabz/serviceTestCases/TestCases.java
@@ -1,5 +1,6 @@
 package com.bridgeLabz.serviceTestCases;
 
+import com.bridgeLabz.Exception.CensusAnalyzerException;
 import com.bridgeLabz.service.StateCensusAnalyzer;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,9 +10,21 @@ import java.io.IOException;
 public class TestCases {
 
     @Test
-    public void givenNumberOfRecords_WhenMatched_ReturnTrue() throws IOException {
-        StateCensusAnalyzer stateCensusAnalyzer = new StateCensusAnalyzer();
+    public void givenNumberOfRecords_WhenMatched_ReturnTrue() throws CensusAnalyzerException {
+        final String CSV_FILE_PATH = "/home/admin2/IndianStatesCensusAnalyzer/src/test/resources/StateCensusData.csv";
+        StateCensusAnalyzer stateCensusAnalyzer = new StateCensusAnalyzer(CSV_FILE_PATH);
         int numberOfRecords = stateCensusAnalyzer.loadRecords();
         Assert.assertEquals(29,numberOfRecords);
+    }
+
+    @Test
+    public void givenFileName_WhenWrong_ReturnCustomiseException(){
+        final String CSV_FILE_PATH = "/home/admin2/IndianStatesCensusAnalyzer/src/test/resources/stateCensusData.csv";
+        StateCensusAnalyzer stateCensusAnalyzer = new StateCensusAnalyzer(CSV_FILE_PATH);
+        try {
+            stateCensusAnalyzer.loadRecords();
+        } catch (CensusAnalyzerException e) {
+            Assert.assertEquals(CensusAnalyzerException.ExceptionType.FILE_NOT_FOUND,e.exceptionType);
+        }
     }
 }
