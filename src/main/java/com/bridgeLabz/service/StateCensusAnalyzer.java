13060d99c1c4f2436cfbf7d54e653b974b02b1ac
vampire16 <siddheshsalvi525@gmai.com>
2020-03-17 12:04:15 +0530

    ADDED : New method to load all records of csv file and return the count of records

--- src/main/java/com/bridgeLabz/service/StateCensusAnalyzer.java
+++ src/main/java/com/bridgeLabz/service/StateCensusAnalyzer.java
@@ -1,7 +1,40 @@
 package com.bridgeLabz.service;
 
+import com.bridgeLabz.model.CSVStateCensus;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
 public class StateCensusAnalyzer {
-    public static void main(String[] args){
-        System.out.println("Welcome");
+        private static final String CSV_FILE_PATH = "/home/admin2/IndianStatesCensusAnalyzer/src/test/resources/StateCensusData.csv";
+        int count=0;
+
+        public int loadRecords() throws IOException {
+            try (
+                    Reader reader = Files.newBufferedReader(Paths.get(CSV_FILE_PATH));
+            ) {
+                CsvToBean<CSVStateCensus> csvToBean = new CsvToBeanBuilder(reader)
+                        .withType(CSVStateCensus.class)
+                        .withIgnoreLeadingWhiteSpace(true)
+                        .build();
+
+                Iterator<CSVStateCensus> csvUserIterator = csvToBean.iterator();
+
+                while (csvUserIterator.hasNext()) {
+                    CSVStateCensus csvStateCensus = csvUserIterator.next();
+                    System.out.println("Name : " + csvStateCensus.getState());
+                    System.out.println("Email : " + csvStateCensus.getPopulation());
+                    System.out.println("Phone : " + csvStateCensus.getArea());
+                    System.out.println("Country : " + csvStateCensus.getDensity());
+                    System.out.println("==========================");
+                    count++;
+                }
+            }
+            return count;
+        }
     }
-}
+
