8de4bc8a37e755a6dfb17506108464ea3582341a
vampire16 <siddheshsalvi525@gmai.com>
2020-03-17 12:57:09 +0530

    ADDED : Customise exception file not found

--- src/main/java/com/bridgeLabz/Exception/CensusAnalyzerException.java
+++ src/main/java/com/bridgeLabz/Exception/CensusAnalyzerException.java
@@ -1,4 +1,13 @@
 package com.bridgeLabz.Exception;
 
-public class CensusAnalyzerException {
+public class CensusAnalyzerException extends Throwable {
+    public enum ExceptionType{
+        FILE_NOT_FOUND
+    }
+
+    public ExceptionType exceptionType;
+
+    public CensusAnalyzerException(ExceptionType exceptionType) {
+        this.exceptionType = exceptionType;
+    }
 }
