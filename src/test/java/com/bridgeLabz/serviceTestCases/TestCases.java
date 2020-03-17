5d387566a173efcadede1e63f4d265d3b917e13b
vampire16 <siddheshsalvi525@gmai.com>
2020-03-17 12:09:03 +0530

    ADDED : New test methods to check number of records

--- src/test/java/com/bridgeLabz/serviceTestCases/TestCases.java
+++ src/test/java/com/bridgeLabz/serviceTestCases/TestCases.java
@@ -1,4 +1,17 @@
 package com.bridgeLabz.serviceTestCases;
 
+import com.bridgeLabz.service.StateCensusAnalyzer;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class TestCases {
+
+    @Test
+    public void givenNumberOfRecords_WhenMatched_ReturnTrue() throws IOException {
+        StateCensusAnalyzer stateCensusAnalyzer = new StateCensusAnalyzer();
+        int numberOfRecords = stateCensusAnalyzer.loadRecords();
+        Assert.assertEquals(29,numberOfRecords);
+    }
 }
