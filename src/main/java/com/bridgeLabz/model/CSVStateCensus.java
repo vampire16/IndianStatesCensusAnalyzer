a4286da608621de5943bc721dbfe907f8ee6074d
vampire16 <siddheshsalvi525@gmai.com>
2020-03-17 12:06:43 +0530

    ADDED : All header of csv file with getter setter methods and constructor

--- src/main/java/com/bridgeLabz/model/CSVStateCensus.java
+++ src/main/java/com/bridgeLabz/model/CSVStateCensus.java
@@ -1,4 +1,60 @@
 package com.bridgeLabz.model;
 
+import com.opencsv.bean.CsvBindByName;
+
 public class CSVStateCensus {
+
+    @CsvBindByName(column = "State")
+    private String state;
+
+    @CsvBindByName(column = "Population")
+    private String population;
+
+    @CsvBindByName(column = "AreaInSqKm")
+    private String area;
+
+    @CsvBindByName(column = "DensityPerSqKm")
+    private String density;
+
+    public CSVStateCensus() {
+    }
+
+    public CSVStateCensus(String state, String population, String area, String density) {
+        this.state = state;
+        this.population = population;
+        this.area = area;
+        this.density = density;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getDensity() {
+        return density;
+    }
+
+    public void setDensity(String density) {
+        this.density = density;
+    }
 }
