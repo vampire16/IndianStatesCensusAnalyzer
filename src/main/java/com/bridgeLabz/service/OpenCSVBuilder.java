594bfe5b3f51a6af03f3bf8920e9a058effa5aae
vampire16 <hakevaibhav16@gmail.com>
2020-03-19 16:59:59 +0530

    ADDED : Override getList method

--- src/main/java/com/bridgeLabz/service/OpenCSVBuilder.java
+++ src/main/java/com/bridgeLabz/service/OpenCSVBuilder.java
@@ -5,10 +5,11 @@ import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public class OpenCSVBuilder<E> implements ICSVBuilder {
 
-//    Iteartor of csv file
+//    ITERATOR OF CSV FILE
     @Override
     public <E> Iterator getIterator(Reader reader, Class csvClass){
         CsvToBean<E> csvToBean = new CsvToBeanBuilder(reader)
@@ -17,5 +18,14 @@ public class OpenCSVBuilder<E> implements ICSVBuilder {
                 .build();
         Iterator<E> csvUserIterator = csvToBean.iterator();
         return csvUserIterator;
+    }
+
+//    LIST OF CSV FILE
+    @Override
+    public <E> List getList(Reader reader, Class csvClass) {
+        CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder(reader)
+                .withType(csvClass)
+                .withIgnoreLeadingWhiteSpace(true);
+        return csvToBeanBuilder.build().parse();
     }
 }
