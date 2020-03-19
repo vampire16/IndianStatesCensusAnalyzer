0fd8a68328aa9552e002c6bf0a012ed567d0f35c
vampire16 <hakevaibhav16@gmail.com>
2020-03-19 17:00:54 +0530

    ADDED : New getList method in interface

--- src/main/java/com/bridgeLabz/service/ICSVBuilder.java
+++ src/main/java/com/bridgeLabz/service/ICSVBuilder.java
@@ -2,7 +2,9 @@ package com.bridgeLabz.service;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public interface ICSVBuilder {
     <E> Iterator getIterator(Reader reader, Class csvClass);
+    <E> List getList(Reader reader, Class csvClass);
 }
