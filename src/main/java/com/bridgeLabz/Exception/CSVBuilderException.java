2827a128ef7994967fce29015b653494c086ad81
vampire16 <hakevaibhav16@gmail.com>
2020-03-19 16:08:45 +0530

    REFACTORED : New exception class CSVBuilderException

--- src/main/java/com/bridgeLabz/Exception/CSVBuilderException.java
+++ src/main/java/com/bridgeLabz/Exception/CSVBuilderException.java
@@ -1,4 +1,15 @@
 package com.bridgeLabz.Exception;
 
-public class CSVBuilderException {
+public class CSVBuilderException extends Exception {
+    public ExceptionType exceptionType;
+
+    public CSVBuilderException(String message, ExceptionType exceptionType) {
+        super(message);
+        this.exceptionType = exceptionType;
+    }
+
+    public enum ExceptionType {
+        FILE_NOT_FOUND,
+        DELIMITER_OR_HEADER_INCORRECT
+    }
 }
