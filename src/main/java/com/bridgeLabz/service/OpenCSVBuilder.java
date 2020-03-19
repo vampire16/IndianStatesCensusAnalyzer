99f63cf42e793b2adae4223ac310e9f3424947f6
vampire16 <hakevaibhav16@gmail.com>
2020-03-19 14:54:51 +0530

    ADDED : Method for iterator of csv file

--- src/main/java/com/bridgeLabz/service/OpenCSVBuilder.java
+++ src/main/java/com/bridgeLabz/service/OpenCSVBuilder.java
@@ -1,4 +1,19 @@
 package com.bridgeLabz.service;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.Reader;
+import java.util.Iterator;
+
 public class OpenCSVBuilder {
+//    Iteartor of csv file
+    public <E> Iterator getIterator(Reader reader, Class csvClass){
+        CsvToBean<E> csvToBean = new CsvToBeanBuilder(reader)
+                .withType(csvClass)
+                .withIgnoreLeadingWhiteSpace(true)
+                .build();
+        Iterator<E> csvUserIterator = csvToBean.iterator();
+        return csvUserIterator;
+    }
 }
