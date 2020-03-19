f91c816c53be205fdc1af99c36a8f7666c1e752d
vampire16 <hakevaibhav16@gmail.com>
2020-03-19 15:41:30 +0530

    ADDED : New interface for getIterator method

--- src/main/java/com/bridgeLabz/service/ICSVBuilder.java
+++ src/main/java/com/bridgeLabz/service/ICSVBuilder.java
@@ -1,4 +1,8 @@
 package com.bridgeLabz.service;
 
+import java.io.Reader;
+import java.util.Iterator;
+
 public interface ICSVBuilder {
+    public <E> Iterator getIterator(Reader reader, Class csvClass);
 }
