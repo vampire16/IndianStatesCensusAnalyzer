c5fdcc7eff109247d5bb57726a300e81f2b886ac
vampire16 <hakevaibhav16@gmail.com>
2020-03-29 15:11:58 +0530

    REFACTORED : Indentation

--- src/main/java/com/bridgeLabz/service/ICSVBuilder.java
+++ src/main/java/com/bridgeLabz/service/ICSVBuilder.java
@@ -1,10 +1,12 @@
 package com.bridgeLabz.service;
 
+import com.bridgeLabz.Exception.CSVBuilderException;
+
 import java.io.Reader;
 import java.util.Iterator;
 import java.util.List;
 
 public interface ICSVBuilder {
-    <E> Iterator getIterator(Reader reader, Class csvClass);
+    <E> Iterator getIterator(Reader reader, Class csvClass) throws CSVBuilderException;
     <E> List getList(Reader reader, Class csvClass);
 }
