3449d2009c84f42863fa13db336ad8c3975e7af4
vampire16 <hakevaibhav16@gmail.com>
2020-03-29 16:13:44 +0530

    ADDED : DAO class for stateCensusPojo and stateCodePojo

--- src/main/java/com/bridgeLabz/service/CensusDAO.java
+++ src/main/java/com/bridgeLabz/service/CensusDAO.java
@@ -1,4 +1,29 @@
 package com.bridgeLabz.service;
 
+import com.bridgeLabz.model.StateCensusPojo;
+import com.bridgeLabz.model.StateCodePojo;
+
 public class CensusDAO {
+    public String state;
+    public int population;
+    public int area;
+    public int density;
+    public String stateCode;
+    public String stateName;
+    public String srNo;
+    public String tin;
+
+    public CensusDAO(StateCensusPojo stateCensusPojo){
+        this.state = stateCensusPojo.state;
+        this.population = stateCensusPojo.population;
+        this.area = stateCensusPojo.area;
+        this.density = stateCensusPojo.density;
+    }
+
+    public CensusDAO(StateCodePojo stateCodePojo){
+        this.stateCode = stateCodePojo.stateCode;
+        this.stateName = stateCodePojo.stateName;
+        this.srNo = stateCodePojo.srNo;
+
+    }
 }
