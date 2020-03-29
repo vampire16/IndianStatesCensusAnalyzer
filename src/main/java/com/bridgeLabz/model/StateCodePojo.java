7e4aeffc7944ff6036b506e064feb09386a17e28
vampire16 <hakevaibhav16@gmail.com>
2020-03-29 15:10:17 +0530

    REFACTORED : Removed unuseful getter setter methods

--- src/main/java/com/bridgeLabz/model/StateCodePojo.java
+++ src/main/java/com/bridgeLabz/model/StateCodePojo.java
@@ -3,17 +3,14 @@ package com.bridgeLabz.model;
 import com.opencsv.bean.CsvBindByName;
 
 public class StateCodePojo {
-    @CsvBindByName(column = "SrNo", required = true)
-    private String srno;
-
-    @CsvBindByName(column = "StateName", required = true)
-    private String stateName;
-
-    @CsvBindByName(column = "TIN", required = true)
-    private String tin;
-
     @CsvBindByName(column = "StateCode", required = true)
     public String stateCode;
+    @CsvBindByName(column = "SrNo", required = true)
+    private String srno;
+    @CsvBindByName(column = "StateName", required = true)
+    private String stateName;
+    @CsvBindByName(column = "TIN", required = true)
+    private String tin;
 
     public StateCodePojo() {
     }
@@ -23,30 +20,6 @@ public class StateCodePojo {
         this.stateName = stateName;
         this.tin = tin;
         this.stateCode = stateCode;
-    }
-
-    public String getSrno() {
-        return srno;
-    }
-
-    public void setSrno(String srno) {
-        this.srno = srno;
-    }
-
-    public String getStateName() {
-        return stateName;
-    }
-
-    public void setStateName(String stateName) {
-        this.stateName = stateName;
-    }
-
-    public String getTin() {
-        return tin;
-    }
-
-    public void setTin(String tin) {
-        this.tin = tin;
     }
 
     public String getStateCode() {
