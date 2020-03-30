13c0ebe4be210a8f9de6c224e9330d5e4dfade01
vampire16 <hakevaibhav16@gmail.com>
2020-03-30 08:25:49 +0530

    ADDED : Pojo class of us census data

--- src/main/java/com/bridgeLabz/model/CSVUSCensus.java
+++ src/main/java/com/bridgeLabz/model/CSVUSCensus.java
@@ -1,4 +1,16 @@
 package com.bridgeLabz.model;
 
+import com.opencsv.bean.CsvBindByName;
+
 public class CSVUSCensus {
+    @CsvBindByName(column = "State Id", required = true)
+    public String usStateId;
+    @CsvBindByName(column = "State", required = true)
+    public String usState;
+    @CsvBindByName(column = "Population", required = true)
+    public String usPopulation;
+    @CsvBindByName(column = "Total area", required = true)
+    public String usArea;
+    @CsvBindByName(column = "Population Density", required = true)
+    public String usPopulationDensity;
 }
